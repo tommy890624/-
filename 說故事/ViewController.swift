e41fa1a1079031414e1594579f65950b31c3d6fe
Tommy <tommy890624@gmail.com>
2024-09-01 19:26:38 +0800

    Initial Commit

---
+++ 說故事/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  說故事
+//
+//  Created by 黃鈺展 on 2024/9/1.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
